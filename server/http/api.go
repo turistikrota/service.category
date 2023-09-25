228a2ff964bc8074dc48e5d06a221ea4d903b9da
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-09-25 19:05:21 +0300

    add http base

--- server/http/api.go
+++ server/http/api.go
@@ -1,1 +1,51 @@
 package http
+
+import "github.com/gofiber/fiber/v2"
+
+func (h srv) CategoryCreate(ctx *fiber.Ctx) error {
+	return nil
+}
+
+func (h srv) CategoryUpdate(ctx *fiber.Ctx) error {
+	return nil
+}
+
+func (h srv) CategoryUpdateOrder(ctx *fiber.Ctx) error {
+	return nil
+}
+
+func (h srv) CategoryEnable(ctx *fiber.Ctx) error {
+	return nil
+}
+
+func (h srv) CategoryDisable(ctx *fiber.Ctx) error {
+	return nil
+}
+
+func (h srv) CategoryDelete(ctx *fiber.Ctx) error {
+	return nil
+}
+
+func (h srv) CategoryAdminView(ctx *fiber.Ctx) error {
+	return nil
+}
+
+func (h srv) CategoryView(ctx *fiber.Ctx) error {
+	return nil
+}
+
+func (h srv) CategoryList(ctx *fiber.Ctx) error {
+	return nil
+}
+
+func (h srv) CategoryListChild(ctx *fiber.Ctx) error {
+	return nil
+}
+
+func (h srv) CategoryAdminList(ctx *fiber.Ctx) error {
+	return nil
+}
+
+func (h srv) CategoryAdminListChild(ctx *fiber.Ctx) error {
+	return nil
+}
--- server/http/http.go
+++ server/http/http.go
@@ -1,1 +1,63 @@
 package http
+
+import (
+	"context"
+
+	"github.com/cilloparch/cillop/helpers/http"
+	"github.com/cilloparch/cillop/i18np"
+	"github.com/cilloparch/cillop/server"
+	"github.com/cilloparch/cillop/validation"
+	"github.com/gofiber/fiber/v2"
+	"github.com/turistikrota/service.category/app"
+	"github.com/turistikrota/service.category/config"
+	"github.com/turistikrota/service.shared/auth/session"
+	"github.com/turistikrota/service.shared/auth/token"
+)
+
+type srv struct {
+	app         app.Application
+	i18n        i18np.I18n
+	validator   validation.Validator
+	ctx         context.Context
+	tknSrv      token.Service
+	sessionSrv  session.Service
+	httpHeaders config.HttpHeaders
+}
+
+type Config struct {
+	App         app.Application
+	I18n        i18np.I18n
+	Validator   validation.Validator
+	Context     context.Context
+	HttpHeaders config.HttpHeaders
+	TokenSrv    token.Service
+	SessionSrv  session.Service
+}
+
+func New(config Config) server.Server {
+	return srv{
+		app:         config.App,
+		i18n:        config.I18n,
+		validator:   config.Validator,
+		ctx:         config.Context,
+		tknSrv:      config.TokenSrv,
+		sessionSrv:  config.SessionSrv,
+		httpHeaders: config.HttpHeaders,
+	}
+}
+
+func (s srv) Listen() error {
+	return nil
+}
+
+func (h srv) parseBody(c *fiber.Ctx, d interface{}) {
+	http.ParseBody(c, h.validator, h.i18n, d)
+}
+
+func (h srv) parseParams(c *fiber.Ctx, d interface{}) {
+	http.ParseParams(c, h.validator, h.i18n, d)
+}
+
+func (h srv) parseQuery(c *fiber.Ctx, d interface{}) {
+	http.ParseQuery(c, h.validator, h.i18n, d)
+}
