5244249f4f65bbd57eaaf19b1412c680f5b5befd
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-09-25 19:19:20 +0300

    add category create http handler

--- server/http/api.go
+++ server/http/api.go
@@ -1,9 +1,21 @@
 package http
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/cilloparch/cillop/result"
+	"github.com/gofiber/fiber/v2"
+	"github.com/turistikrota/service.category/app/command"
+	"github.com/turistikrota/service.shared/server/http/auth/current_user"
+)
 
 func (h srv) CategoryCreate(ctx *fiber.Ctx) error {
-	return nil
+	cmd := command.CategoryCreateCmd{}
+	h.parseBody(ctx, &cmd)
+	cmd.AdminUUID = current_user.Parse(ctx).UUID
+	res, err := h.app.Commands.CategoryCreate(ctx.UserContext(), cmd)
+	if err != nil {
+		return result.Error(h.i18n.TranslateFromError(*err))
+	}
+	return result.SuccessDetail(Messages.Success.CategoryCreated, res)
 }
 
 func (h srv) CategoryUpdate(ctx *fiber.Ctx) error {
--- server/http/msg.go
+++ server/http/msg.go
@@ -1,1 +1,45 @@
 package http
+
+type successMessages struct {
+	CategoryCreated      string
+	CategoryUpdated      string
+	CategoryOrderUpdated string
+	CategoryDeleted      string
+	CategoryEnabled      string
+	CategoryDisabled     string
+	CategoryView         string
+	CategoryAdminView    string
+	CategoryList         string
+	CategoryListChild    string
+}
+
+type errorMessages struct {
+	RequiredAuth      string
+	CurrentUserAccess string
+	AdminRoute        string
+}
+
+type messages struct {
+	Success successMessages
+	Error   errorMessages
+}
+
+var Messages = messages{
+	Success: successMessages{
+		CategoryCreated:      "http_success_category_created",
+		CategoryUpdated:      "http_success_category_updated",
+		CategoryOrderUpdated: "http_success_category_order_updated",
+		CategoryDeleted:      "http_success_category_deleted",
+		CategoryEnabled:      "http_success_category_enabled",
+		CategoryDisabled:     "http_success_category_disabled",
+		CategoryView:         "http_success_category_view",
+		CategoryAdminView:    "http_success_category_admin_view",
+		CategoryList:         "http_success_category_list",
+		CategoryListChild:    "http_success_category_list_child",
+	},
+	Error: errorMessages{
+		RequiredAuth:      "http_error_required_auth",
+		CurrentUserAccess: "http_error_current_user_access",
+		AdminRoute:        "http_error_admin_route",
+	},
+}
