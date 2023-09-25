3423c1ac995b691f5e49fbada715636747a23f2b
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-09-25 13:53:05 +0300

    add roles

--- config/roles.go
+++ config/roles.go
@@ -1,1 +1,35 @@
 package config
+
+import "github.com/turistikrota/service.shared/base_roles"
+
+type categoryRoles struct {
+	Create       string
+	Update       string
+	Enable       string
+	Disable      string
+	Delete       string
+	ViewAdmin    string
+	List         string
+	ListChildren string
+	ReOrder      string
+}
+
+type roles struct {
+	base_roles.Roles
+	Category categoryRoles
+}
+
+var Roles = roles{
+	Roles: base_roles.BaseRoles,
+	Category: categoryRoles{
+		Create:       "category.create",
+		Update:       "category.update",
+		Enable:       "category.enable",
+		Disable:      "category.disable",
+		Delete:       "category.delete",
+		ViewAdmin:    "category.view.admin",
+		List:         "category.list",
+		ListChildren: "category.list.children",
+		ReOrder:      "category.reorder",
+	},
+}
