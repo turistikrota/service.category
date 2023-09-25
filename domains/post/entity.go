d8aec6fc7bd72496ae2ed04559f570f6317d6e7c
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-09-25 13:13:12 +0300

    add post domain

---
+++ domains/post/entity.go
@@ -0,0 +1,128 @@
+package post
+
+import (
+	"time"
+
+	"github.com/cilloparch/cillop/i18np"
+)
+
+type ValidationError struct {
+	Field   string  `json:"field"`
+	Message string  `json:"message"`
+	Params  i18np.P `json:"params"`
+}
+
+type UserDetail struct {
+	UUID string `json:"uuid"`
+	Name string `json:"name"`
+	Code string `json:"code"`
+}
+
+type Entity struct {
+	UUID          string          `json:"uuid"`
+	Owner         Owner           `json:"owner"`
+	Images        []Image         `json:"images"`
+	Meta          map[Locale]Meta `json:"meta"`
+	CategoryUUIDs []string        `json:"categoryUUIDs"`
+	Features      []Feature       `json:"features"`
+	Prices        []Price         `json:"prices"`
+	Location      Location        `json:"location"`
+	Boosts        []Boost         `json:"boosts"`
+	People        People          `json:"people"`
+	Type          Type            `json:"type"`
+	Order         int             `json:"order"`
+	IsActive      bool            `json:"isActive"`
+	IsDeleted     bool            `json:"isDeleted"`
+	IsValid       bool            `json:"isValid"`
+	CreatedAt     time.Time       `json:"createdAt"`
+	UpdatedAt     time.Time       `json:"updatedAt"`
+}
+type Owner struct {
+	UUID     string `json:"uuid"`
+	NickName string `json:"nickName"`
+}
+
+type Image struct {
+	Url   string `json:"url"`
+	Order int16  `json:"order"`
+}
+
+type People struct {
+	MinAdult int `json:"minAdult"`
+	MaxAdult int `json:"maxAdult"`
+	MinKid   int `json:"minKid"`
+	MaxKid   int `json:"maxKid"`
+	MinBaby  int `json:"minBaby"`
+	MaxBaby  int `json:"maxBaby"`
+}
+
+type Meta struct {
+	Locale      string `json:"locale"`
+	Description string `json:"description"`
+	Title       string `json:"title"`
+	Slug        string `json:"slug"`
+	MarkdownURL string `json:"markdownURL"`
+}
+
+type Feature struct {
+	CategoryInputUUID string      `json:"categoryInputUUID"`
+	Value             interface{} `json:"value"`
+	IsPayed           bool        `json:"isPayed"`
+}
+
+type Price struct {
+	StartDate           time.Time `json:"startDate"`
+	EndDate             time.Time `json:"endDate"`
+	Price               float64   `json:"price"`
+	Currencies          []string  `json:"currencies"`
+	IsCurrencyProtected bool      `json:"isCurrencyProtected"`
+}
+
+type Location struct {
+	Country     string    `json:"country"`
+	City        string    `json:"city"`
+	Street      string    `json:"street"`
+	Address     string    `json:"address"`
+	IsStrict    bool      `json:"isStrict"`
+	Coordinates []float64 `json:"coordinates"`
+}
+
+type Boost struct {
+	UUID      string    `json:"uuid"`
+	StartDate time.Time `json:"startDate"`
+	EndDate   time.Time `json:"endDate"`
+}
+
+type Type string
+
+type Locale string
+
+const (
+	LocaleEN Locale = "en"
+	LocaleTR Locale = "tr"
+)
+
+const (
+	TypeEstate     Type = "estate"
+	TypeCar        Type = "car"
+	TypeBoat       Type = "boat"
+	TypeMotorcycle Type = "motorcycle"
+	TypeOther      Type = "other"
+)
+
+func (t Type) String() string {
+	return string(t)
+}
+
+func (e Entity) GetFeatureByCategoryInputUUID(uuid string) (Feature, int, bool) {
+	for idx, f := range e.Features {
+		if f.CategoryInputUUID == uuid {
+			return f, idx, true
+		}
+	}
+	return Feature{}, -1, false
+}
+
+func (l Locale) String() string {
+	return string(l)
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module github.com/turistikrota/service.category
 
 go 1.20
+
+require github.com/cilloparch/cillop v0.0.3
+
+require (
+	github.com/BurntSushi/toml v1.3.2 // indirect
+	github.com/nicksnyder/go-i18n/v2 v2.2.1 // indirect
+	golang.org/x/text v0.12.0 // indirect
+)
