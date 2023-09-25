7dec3d011e80f811011447e5fbf74ae5454766cd
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-09-25 18:49:55 +0300

    add cqrs app interface

--- app/app.go
+++ app/app.go
@@ -1,1 +1,30 @@
 package app
+
+import (
+	"github.com/turistikrota/service.category/app/command"
+	"github.com/turistikrota/service.category/app/query"
+)
+
+type Application struct {
+	Commands Commands
+	Queries  Queries
+}
+
+type Commands struct {
+	CategoryCreate       command.CategoryCreateHandler
+	CategoryUpdate       command.CategoryUpdateHandler
+	CategoryDelete       command.CategoryDeleteHandler
+	CategoryDisable      command.CategoryDisableHandler
+	CategoryEnable       command.CategoryEnableHandler
+	CategoryUpdateOrder  command.CategoryUpdateOrderHandler
+	CategoryValidatePost command.CategoryValidatePostHandler
+}
+
+type Queries struct {
+	CategoryFind           query.CategoryFindHandler
+	CategoryFindBySlug     query.CategoryFindBySlugHandler
+	CategoryFindChild      query.CategoryFindChildHandler
+	CategoryFindAll        query.CategoryFindAllHandler
+	CategoryAdminFindChild query.CategoryAdminFindChildHandler
+	CategoryAdminFindAll   query.CategoryAdminFindAllHandler
+}
