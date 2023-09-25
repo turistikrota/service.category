4a9dac845ac651b4e655f57d4f59324c7021f682
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-09-25 21:56:04 +0300

    add http base functions

--- go.mod
+++ go.mod
@@ -30,8 +30,10 @@ require (
 	github.com/mattn/go-runewidth v0.0.14 // indirect
 	github.com/montanaflynn/stats v0.7.0 // indirect
 	github.com/nicksnyder/go-i18n/v2 v2.2.1 // indirect
+	github.com/philhofer/fwd v1.1.2 // indirect
 	github.com/rivo/uniseg v0.4.4 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
+	github.com/tinylib/msgp v1.1.8 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.48.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
--- server/http/http.go
+++ server/http/http.go
@@ -2,16 +2,27 @@ package http
 
 import (
 	"context"
+	"time"
+
+	httpServer "github.com/turistikrota/service.shared/server/http"
 
 	"github.com/cilloparch/cillop/helpers/http"
 	"github.com/cilloparch/cillop/i18np"
 	"github.com/cilloparch/cillop/server"
 	"github.com/cilloparch/cillop/validation"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/limiter"
+	"github.com/gofiber/fiber/v2/middleware/timeout"
 	"github.com/turistikrota/service.category/app"
 	"github.com/turistikrota/service.category/config"
 	"github.com/turistikrota/service.shared/auth/session"
 	"github.com/turistikrota/service.shared/auth/token"
+	"github.com/turistikrota/service.shared/server/http/auth"
+	"github.com/turistikrota/service.shared/server/http/auth/claim_guard"
+	"github.com/turistikrota/service.shared/server/http/auth/current_user"
+	"github.com/turistikrota/service.shared/server/http/auth/device_uuid"
+	"github.com/turistikrota/service.shared/server/http/auth/required_access"
 )
 
 type srv struct {
@@ -60,4 +71,64 @@ func (h srv) parseParams(c *fiber.Ctx, d interface{}) {
 
 func (h srv) parseQuery(c *fiber.Ctx, d interface{}) {
 	http.ParseQuery(c, h.validator, h.i18n, d)
+}
+
+func (h srv) currentUserAccess() fiber.Handler {
+	return current_user.New(current_user.Config{
+		TokenSrv:   h.tknSrv,
+		SessionSrv: h.sessionSrv,
+		I18n:       &h.i18n,
+		MsgKey:     Messages.Error.CurrentUserAccess,
+		HeaderKey:  httpServer.Headers.Authorization,
+		CookieKey:  auth.Cookies.AccessToken,
+		UseCookie:  true,
+		UseBearer:  true,
+		IsRefresh:  false,
+		IsAccess:   true,
+	})
+}
+
+func (h srv) rateLimit() fiber.Handler {
+	return limiter.New(limiter.Config{
+		Max:        50,
+		Expiration: 5 * time.Minute,
+	})
+}
+
+func (h srv) deviceUUID() fiber.Handler {
+	return device_uuid.New(device_uuid.Config{
+		Domain: h.httpHeaders.Domain,
+	})
+}
+
+func (h srv) requiredAccess() fiber.Handler {
+	return required_access.New(required_access.Config{
+		I18n:   h.i18n,
+		MsgKey: Messages.Error.RequiredAuth,
+	})
+}
+
+func (h srv) adminRoute(extra ...string) fiber.Handler {
+	claims := []string{config.Roles.Admin}
+	if len(extra) > 0 {
+		claims = append(claims, extra...)
+	}
+	return claim_guard.New(claim_guard.Config{
+		Claims: claims,
+		I18n:   h.i18n,
+		MsgKey: Messages.Error.AdminRoute,
+	})
+}
+
+func (h srv) cors() fiber.Handler {
+	return cors.New(cors.Config{
+		AllowOrigins:     h.httpHeaders.AllowedOrigins,
+		AllowMethods:     h.httpHeaders.AllowedMethods,
+		AllowHeaders:     h.httpHeaders.AllowedHeaders,
+		AllowCredentials: h.httpHeaders.AllowCredentials,
+	})
+}
+
+func (h srv) wrapWithTimeout(fn fiber.Handler) fiber.Handler {
+	return timeout.NewWithContext(fn, 10*time.Second)
 }
